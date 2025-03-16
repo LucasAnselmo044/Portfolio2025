281f22c5a259eee2203adb777901de717ac4ebdd
Lucas <xablau2007pvp@gmail.com>
2025-03-16 18:00:48 -0300

    UI Fix

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function enviarWpp(event) {
+  event.preventDefault();
 
-event.preventDefault()
+  const nome = document.getElementById("nome").value;
+  const mensagem = document.getElementById("mensagem").value;
+  const email = document.getElementById("email").value;
+  const telefone = "5581983406667";
 
-const nome = document.getElementById('nome').value;
-const mensagem = document.getElementById('mensagem').value;
-const email = document.getElementById('email').value;
-const telefone = '5581983406667'
+  const texto = `Olá! Me chamo ${nome}, ${mensagem}, meu email para contato: ${email}`;
+  const msgFormatada = encodeURIComponent(texto);
 
-const texto = `Olá! Me chamo ${nome}, ${mensagem}, meu email para contato: ${email}`
-const msgFormatada = encodeURIComponent(texto)
+  const url = `https://wa.me/${telefone}/?text=${msgFormatada}`;
 
-const url = `https://wa.me/${telefone}/?text=${msgFormatada}`
+  console.log(url);
 
-console.log(url)
-
-window.open(url, '_blank')
+  window.open(url, "_blank");
 }
